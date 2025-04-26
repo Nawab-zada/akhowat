001abf0faa90dc4fe8ba1ac2740816de4c8c8382
nawab zada <zada86829@gmail.com>
2025-04-26 19:47:45 +0500

    updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script"; // ✅ Import Script component
 import "./globals.css";
 
 const geistSans = Geist({
@@ -24,6 +25,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+  
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17033123168"
+          strategy="afterInteractive"
+        />
+        <Script id="google-ads-gtag" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-17033123168');
+          `}
+        </Script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
